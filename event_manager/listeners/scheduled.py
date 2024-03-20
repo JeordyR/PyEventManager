b672fde1980e7dddf5c85ec1f24c7712f209006c
Jeordy <JeordyR@users.noreply.github.com>
2024-03-20 10:09:29 -0400

    feat: Add ability to stop scheduled listeners.

--- event_manager/listeners/scheduled.py
+++ event_manager/listeners/scheduled.py
@@ -1,16 +1,16 @@
 import logging
-import time
+import multiprocessing
+import threading
 from collections.abc import Callable
 from datetime import timedelta
-from multiprocessing import Process
-from threading import Thread
+from multiprocessing.synchronize import Event
 
 from event_manager.listeners.base import BaseListener
 
 logger = logging.getLogger("event_manager")
 
 
-def run(interval: timedelta, func: Callable):
+def run(interval: timedelta, func: Callable, event: Event | threading.Event):
     """
     Run the provided function on the provided interval.
 
@@ -18,8 +18,7 @@ def run(interval: timedelta, func: Callable):
         interval (timedelta): Inteveral to run the stored function.
         func (Callable): Function to run on schedule.
     """
-    while True:
-        time.sleep(interval.total_seconds())
+    while not event.wait(interval.total_seconds()):
         logger.debug(f"Running {func.__name__} on schedule.")
         func()
 
@@ -29,7 +28,7 @@ class ScheduledListener(BaseListener):
         self,
         interval: timedelta,
         func: Callable[[None], None],
-        fork_type: type[Thread | Process] = Process,
+        fork_type: type[threading.Thread | multiprocessing.Process] = multiprocessing.Process,
     ):
         """
         Class for a basic listener in the event management system.
@@ -44,6 +43,9 @@ class ScheduledListener(BaseListener):
         self.interval = interval
         self.func = func
         self.fork_type = fork_type
+        self.sync_event: Event | threading.Event = (
+            threading.Event() if fork_type == threading.Thread else multiprocessing.Event()
+        )
 
     def __call__(self):
         """
@@ -53,6 +55,20 @@ class ScheduledListener(BaseListener):
             data (BaseModel): Data to pass to the invoked function.
         """
         logger.debug(f"Executing {self.func.__name__}... Set to run every {self.interval.total_seconds()} seconds.")
-        fork = self.fork_type(target=run, kwargs={"interval": self.interval, "func": self.func})
+        fork = self.fork_type(
+            target=run,
+            kwargs={
+                "interval": self.interval,
+                "func": self.func,
+                "event": self.sync_event,
+            },
+        )
         fork.daemon = True
         fork.start()
+
+    def stop(self):
+        """
+        Stop the scheduled listener.
+        """
+        self.sync_event.set()
+        logger.debug(f"Stopping {self.func.__name__} from running on schedule.")
